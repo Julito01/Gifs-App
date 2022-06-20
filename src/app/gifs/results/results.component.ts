91cda6b93b2aeea01dab58d0c131b94e8a658307
Julio Dechert <julitod55@hotmail.com>
2022-06-20 19:32:29 -0300

    Getter of gifs

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { Gif } from '../interface/gifs.interface';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
 })
-export class ResultsComponent {}
+export class ResultsComponent {
+  get results(): Gif[] {
+    return this.gifService.results;
+  }
+
+  constructor(private gifService: GifsService) {}
+}
