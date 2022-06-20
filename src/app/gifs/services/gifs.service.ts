cbc6ae7e971e36cc15600fdd9334d442fa82a61a
Julio Dechert <julitod55@hotmail.com>
2022-06-20 20:25:32 -0300

    Implemented LocalStorage and HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, GifsResponse } from '../interface/gifs.interface';
 
@@ -7,7 +7,7 @@ import { Gif, GifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
   private apiKey: string = 'fqgHzpm4wej3elMG8SN1Gj1310NuTqB2';
-  private apiUrl: string = 'https://api.giphy.com/v1/gifs/search';
+  private apiUrl: string = 'https://api.giphy.com/v1/gifs';
   private _history: string[] = [];
   public results: Gif[] = [];
 
@@ -15,7 +15,13 @@ export class GifsService {
     return [...this._history];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._history = JSON.parse(localStorage.getItem('history')!) || [];
+    this.results = JSON.parse(localStorage.getItem('results')!) || [];
+    // if (localStorage.getItem('history')) {
+    //   this._history = JSON.parse(localStorage.getItem('history')!);
+    // }
+  }
 
   searchGifs(query: string = '') {
     query = query.trim().toLowerCase();
@@ -23,17 +29,21 @@ export class GifsService {
     if (!this._history.includes(query)) {
       this._history.unshift(query);
       this._history = this._history.splice(0, 7);
+      localStorage.setItem('history', JSON.stringify(this._history));
     }
 
-    this.http
-      .get<GifsResponse>(
-        `${this.apiUrl}?api_key=${this.apiKey}&q=${query}&limit=30`
-      )
-      .subscribe((response) => {
-        console.log(response.data);
-        this.results = response.data;
-      });
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', query)
+      .set('limit', '30');
 
-    console.log(this._history);
+    console.log(params.toString());
+
+    this.http
+      .get<GifsResponse>(`${this.apiUrl}/search`, { params })
+      .subscribe((response) => {
+        this.results = response.data;
+        localStorage.setItem('results', JSON.stringify(this.results));
+      });
   }
 }
